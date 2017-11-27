85770d9e38b4e27f832e72e6a682b7e7b60bc845
salaleser <salaleser@users.noreply.github.com>
2017-11-27 19:52:47 +0300

    fix

--- src/ru/salaleser/vacdbot/bot/task/InviteToVoiceChatTask.java
+++ src/ru/salaleser/vacdbot/bot/task/InviteToVoiceChatTask.java
@@ -1,10 +1,7 @@
 package ru.salaleser.vacdbot.bot.task;
 
 import ru.salaleser.vacdbot.bot.Bot;
-import sx.blah.discord.handle.obj.IChannel;
-import sx.blah.discord.handle.obj.IUser;
-import sx.blah.discord.handle.obj.IVoiceChannel;
-import sx.blah.discord.handle.obj.StatusType;
+import sx.blah.discord.handle.obj.*;
 
 import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
@@ -14,12 +11,13 @@ public class InviteToVoiceChatTask extends TimerTask {
 	private static int c = 0;
 	private static final IChannel channel = Bot.channelKTOGeneral;
 	private static final IVoiceChannel voice = Bot.voiceChannelGeneral;
+	private static final IRole officer = Bot.roleOfficers;
 
 	public void run() {
 		c++;
 		for (IUser userHere : channel.getUsersHere()) {
 			if (userHere.getPresence().getStatus() != StatusType.OFFLINE && !userHere.isBot() &&
-					userHere.hasRole(Bot.roleOfficers) && !voice.getConnectedUsers().contains(userHere)) {
+					userHere.hasRole(officer) && !voice.getConnectedUsers().contains(userHere)) {
 				channel.sendMessage(userHere +
 						", для вас последнее китайское приглашение в голосовой чат номер " + c);
 				try {
