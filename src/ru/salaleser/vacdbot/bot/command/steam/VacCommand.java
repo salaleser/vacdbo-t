0ce9cd7c951621fb21749603ac2c07c3807d0916
salaleser <salaleser@users.noreply.github.com>
2018-02-04 16:22:19 +0300

    fixed and added discordid option

--- src/ru/salaleser/vacdbot/bot/command/steam/VacCommand.java
+++ src/ru/salaleser/vacdbot/bot/command/steam/VacCommand.java
@@ -1,9 +1,11 @@
 package ru.salaleser.vacdbot.bot.command.steam;
 
 import ru.salaleser.vacdbot.*;
+import ru.salaleser.vacdbot.bot.Bot;
 import ru.salaleser.vacdbot.bot.command.Command;
 import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.handle.obj.IMessage;
+import sx.blah.discord.handle.obj.IUser;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -21,10 +23,10 @@ public class VacCommand extends Command {
 	public void help(IMessage message) {
 		message.getChannel().sendMessage(buildHelp(
 				"Показывает забаненных друзей пользователя за указанный период времени.",
-				"`~vac [<количество_дней> [<SteamID64>]]`.",
+				"`~vac [<количество_дней> [<SteamID64> | <пользователь_Discord>]]`.",
 				"`~vac` — ваши друзья, получившие бан за прошедший день;\n" +
 						"`~vac <количество_дней>` — ваши друзья, получившие бан за указанное количество дней.",
-				"`~vac 3 76561198095972970`.",
+				"`~vac 3 76561198095972970`, `~vac 30 @salaleser`.",
 				"день может быть от 1 до 9999."
 				)
 		);
@@ -34,7 +36,8 @@ public class VacCommand extends Command {
 	public void handle(IMessage message, String[] args) {
 		//defaults:
 		IChannel channel = message.getChannel();
-		String steamid = Util.getSteamidByDiscordUser(message.getAuthor().getStringID());
+		IUser user = message.getAuthor();
+		String steamid = Util.getSteamidByDiscordid(user.getStringID());
 		int days = 1;
 
 		if (args.length > 0) {
@@ -43,35 +46,47 @@ public class VacCommand extends Command {
 				if (args.length > 1) {
 					if (Util.isSteamID64(args[1])) {
 						steamid = args[1];
+						String discordid = Util.getDiscordidBySteamid(steamid);
+						if (!discordid.equals("noname")) {
+							user = Bot.guildKTO.getClient().getUserByID(Long.parseLong(discordid));
+						}
+					} else if (Util.isDiscordUser(args[1])) {
+						String discordid = args[1].replaceAll("[<@!>]", "");
+						user = Bot.guildKTO.getClient().getUserByID(Long.parseLong(discordid));
+						steamid = Util.getSteamidByDiscordid(discordid);
 					} else {
-						message.reply(Util.b("ошибка в профиле") + "(проверяю баны друзей salaleser'а)");
+						message.reply(Util.b("ошибка в SteamID") + " (установлен SteamID: " + steamid + ")");
 					}
 				} else {
-					message.reply(Util.b("профиль не задан") + " (проверяю баны друзей salaleser'а)");
+					message.reply(Util.b("профиль не задан") + " (установлен SteamID: " + steamid + ")");
 				}
 			} else {
-				message.reply(Util.b("ошибка в количестве дней") + " (проверяю баны за сегодня)");
+				message.reply(Util.b("ошибка в количестве дней") +
+						" (проверяю баны " + user.getName() + " за " + days + " д" + Util.ending(days));
 			}
 		} else {
-			message.reply(Util.b("аргументы не заданы") + " (проверяю баны друзей salaleser'а за сегодня)");
+			message.reply(Util.b("аргументы не заданы") + " (установлены значения: пользователь " +
+					user.getName() + ", период времени: " + days + " д" + Util.ending(days));
 		}
 
-		String name = message.getAuthor().getName();
-		message.getChannel().sendMessage("Проверяю " +
-				"друзей " + name + "...");
+		channel.sendMessage("Проверяю друзей " + user.getName() + "…");
+		if (steamid.equals("noname") || !DBHelper.isExist("users", "steamid", steamid)) {
+			channel.sendMessage(Util.i("С пользователем " + user.getName() +
+					" не ассоциирован SteamID, взаимодействие со Steam API невозможно."));
+			return;
+		}
 		StringBuilder jsonFriends = httpClient.connect("http://api.steampowered.com/" +
 				"ISteamUser/GetFriendList/v0001/" + "?key=" + Config.getSteamWebApiKey() + "&steamid=" +
 				steamid + "&relationship=friend");
 		if (jsonFriends == null) {
-			message.getChannel().sendMessage("*Время ожидания вышло! Повторите запрос...*");
+			channel.sendMessage(Util.i("Время ожидания вышло! Повторите запрос..."));
 			return;
 		}
 		ParserFriends parserFriends = new ParserFriends();
 		ArrayList<StringBuilder> hundredsOfSteamIDs = parserFriends.parse(jsonFriends);
 
-		message.getChannel().sendMessage("Всего друзей: " +
-				Util.b(hundredsOfSteamIDs.get(hundredsOfSteamIDs.size() - 1).toString()) +
-				"\nПроверяю друзей на баны...");
+		channel.sendMessage("Всего друзей: " +
+				Util.b(hundredsOfSteamIDs.get(hundredsOfSteamIDs.size() - 1).toString()) + "\nПроверяю друзей на баны…");
 		ParserFriendsBans parserBans = new ParserFriendsBans();
 		HashMap<String, Integer> cheaters = new HashMap<>();
 		//не забыть исключить из парсинга последний элемент массива (количество друзей)
@@ -80,7 +95,7 @@ public class VacCommand extends Command {
 					"ISteamUser/GetPlayerBans/v1/?key=" + Config.getSteamWebApiKey() + "&steamids=" +
 					hundredsOfSteamIDs.get(i));
 			if (jsonBans == null) {
-				message.getChannel().sendMessage(Util.i("Ошибка HTTP-соединения на " + (i + 1) +
+				channel.sendMessage(Util.i("Ошибка HTTP-соединения на " + (i + 1) +
 						"-ой итерации! Повторяю запрос..."));
 				i--;
 			} else {
@@ -89,7 +104,7 @@ public class VacCommand extends Command {
 		}
 
 		int lastOnesNumber = 0;
-		StringBuilder bMessage = new StringBuilder("Профили друзей " + name + ", получивших бан за последние " +
+		StringBuilder bMessage = new StringBuilder("Профили друзей " + user.getName() + ", получивших бан за последние " +
 				days + " д" + Util.ending(days));
 		for (Map.Entry<String, Integer> lastOne : cheaters.entrySet()) {
 			if (lastOne.getValue() < days) {
@@ -101,7 +116,7 @@ public class VacCommand extends Command {
 		if (lastOnesNumber > 0) {
 			channel.sendMessage(String.valueOf(bMessage)); //fixme hardcode повтор кода
 		} else {
-			channel.sendMessage(Util.b("Забаненных друзей " + name + " за последние " + days + " д" + Util.ending(days) + " нет. Пока нет..."));
+			channel.sendMessage(Util.b("Забаненных друзей " + user.getName() + " за последние " + days + " д" + Util.ending(days) + " нет. Пока нет..."));
 		}
 	}
 }
