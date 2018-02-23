51bd48e23ff48d42a059cdaff3d7f6aba3249e95
salaleser <salaleser@users.noreply.github.com>
2018-02-23 12:42:23 +0300

    added multilanguage support (zn-cn, en-us, ru-ru)

--- src/ru/salaleser/vacdbot/bot/command/TTSCommand.java
+++ src/ru/salaleser/vacdbot/bot/command/TTSCommand.java
@@ -1,14 +1,17 @@
 package ru.salaleser.vacdbot.bot.command;
 
+import com.vdurmont.emoji.Emoji;
 import com.voicerss.tts.*;
 import ru.salaleser.vacdbot.*;
-import sx.blah.discord.handle.obj.IGuild;
-import sx.blah.discord.handle.obj.IMessage;
+import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.api.IShard;
+import sx.blah.discord.handle.obj.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 
 public class TTSCommand extends Command {
 
@@ -32,6 +35,22 @@ public class TTSCommand extends Command {
 					Util.b((folderSize / 1024 / 1024) + " Мбайт")));
 			return;
 		}
+
+		//определяю язык, инглиш по умолчанию:
+		// TODO: 23.02.2018 научить бота определять составные фразы из разных языков, сейчас фраза должна состоять из слов одного языка
+		String language = Languages.English_UnitedStates;
+		if (args[0].matches("^[\\u4E00-\\u9FA5]+$")) language = Languages.Chinese_China;
+		if (args[0].matches("^[А-Яа-яЁё]+$")) language = Languages.Russian;
+		if (args[0].matches("^\\W+$")) language = Languages.Russian; //для символов только русский для смеху
+		if (args[0].matches("^\\d+$")) language = Languages.Russian; //для чисел тоже русский
+
+		//добавлю эмоции с флагами по фану:
+		switch (language) {
+			case "ru-ru": message.addReaction("\uD83C\uDDF7\uD83C\uDDFA"); break;
+			case "en-us": message.addReaction("\uD83C\uDDFA\uD83C\uDDF8"); break;
+			case "zh-cn": message.addReaction("\uD83C\uDDE8\uD83C\uDDF3"); break;
+		}
+
 		String text = String.join(" ", args);
 		String filename;
 		//если запись уже существует, то не стоит тревожить лишний раз синтезатор, тем более, что он платный:
@@ -47,7 +66,7 @@ public class TTSCommand extends Command {
 			row[2] = String.valueOf(++counter);
 			//пихаю обратно:
 			DBHelper.update(TABLE, row);
-		} else {
+		} else { //если же этот набор символов впервые запущен, то сгенерирую имя файла:
 			String id = DBHelper.getNewId(TABLE, "filename");
 			String zeroes;
 			switch (String.valueOf(id).length()) {
@@ -58,10 +77,8 @@ public class TTSCommand extends Command {
 				default: zeroes = "";
 			}
 			filename = zeroes + id;
-			String language = Languages.Russian;
-//			if (Util.isRussian(args[0])) language = Languages.Russian;
-			VoiceProvider voiceProvider = new VoiceProvider(Config.getVoiceRssApiKey());
 
+			VoiceProvider voiceProvider = new VoiceProvider(Config.getVoiceRssApiKey());
 			VoiceParameters params = new VoiceParameters(text, language);
 			params.setCodec(AudioCodec.WAV);
 			params.setFormat(AudioFormat.Format_44KHZ.AF_44khz_16bit_stereo);
@@ -100,6 +117,7 @@ public class TTSCommand extends Command {
 				e.printStackTrace();
 			}
 		}
+
 		Player.queueFile(guild, PATH + filename + EXTENSION);
 	}
 }
