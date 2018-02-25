2c58438153e0bc6b921df4be48a5435c5c90e3ad
salaleser <salaleser@users.noreply.github.com>
2018-02-26 00:40:23 +0300

    initial

---
+++ src/ru/salaleser/vacdbot/bot/TTSColumns.java
@@ -0,0 +1,11 @@
+package ru.salaleser.vacdbot.bot;
+
+//здесь храню номера колонок таблицы "events", они меняются иногда у меня, ведь я еще не в полной мере понимаю чего хочу
+public class TTSColumns {
+	public static int Text = 0;
+	public static int Filename = 1;
+	public static int Counter = 2;
+	public static int Language = 3;
+	public static int Updated = 4;
+}
+// ЭТА ДЛИННАЯ СТРОКА НУЖНА ДЛЯ ТОГО, ЧТОБЫ ПОЯВИЛАСЬ ВОЗМОЖНОСТЬ ГОРИЗОНТАЛЬНО СКРОЛЛИТЬ ДЛЯ ДИСПЛЕЯ С МАЛЕНЬКОЙ ДИАГОНАЛЬЮ, НАПРИМЕР ДЛЯ МОЕГО ОДИННАДЦАТИДЮЙМОВОГО МАКБУКА ЭЙР
