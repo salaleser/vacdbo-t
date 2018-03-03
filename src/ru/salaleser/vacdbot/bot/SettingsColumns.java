843113e88167b0d5988edba67a3f15e94eed5570
salaleser <salaleser@users.noreply.github.com>
2018-03-03 20:35:26 +0300

    init

---
+++ src/ru/salaleser/vacdbot/bot/SettingsColumns.java
@@ -0,0 +1,9 @@
+package ru.salaleser.vacdbot.bot;
+
+public class SettingsColumns {
+	public static int GuildID = 0;
+	public static int Command = 1;
+	public static int Key = 2;
+	public static int Value = 3;
+}
+// ЭТА ДЛИННАЯ СТРОКА НУЖНА ДЛЯ ТОГО, ЧТОБЫ ПОЯВИЛАСЬ ВОЗМОЖНОСТЬ ГОРИЗОНТАЛЬНО СКРОЛЛИТЬ ДЛЯ ДИСПЛЕЯ С МАЛЕНЬКОЙ ДИАГОНАЛЬЮ, НАПРИМЕР ДЛЯ МОЕГО ОДИННАДЦАТИДЮЙМОВОГО МАКБУКА ЭЙР
